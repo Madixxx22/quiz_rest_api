284539ed89b992dd31a19315fd32df6b5113fdd7
Dazai-Os <vysokolyan2016@gmail.com>
2022-05-02 00:17:25 +0300

    В docker-compose добавлена postgresqlb и СУБД Pgadmin, и подключение к БД

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 
+from db.base import database, metadata, engine
 app = FastAPI()
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+    metadata.create_all(bind=engine)
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect() 
 
 @app.get("/")
 async def test():
